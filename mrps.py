754f5ea2b204ed180d63955da84300815d3f83cc
Chris Johnston <40300884+cj667113@users.noreply.github.com>
2021-08-30 10:14:21 -0500

    Add files via upload

--- mrps.py
+++ mrps.py
@@ -16,5 +16,8 @@ sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 sock.bind((UDP_IP,UDP_PORT))
 while True:
-	data,addr = sock.recvfrom(1024)
-	sock.sendto(MESSAGE,addr)
+	try:
+		data,addr = sock.recvfrom(1024)
+		sock.sendto(MESSAGE,addr)
+	except:
+		pass
--- umrpc.py
+++ umrpc.py
@@ -1,8 +1,10 @@
-import socket
-UDP_IP="255.255.255.255"
-UDP_PORT=5005
-MESSAGE=b"MQTT Resolution"
-sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-sock.sendto(MESSAGE,(UDP_IP,UDP_PORT))
-data,addr = sock.recvfrom(1024)
-print(data)
+import usocket as socket
+def mrpc():
+	UDP_IP="255.255.255.255"
+	UDP_PORT=5005
+	MESSAGE=b"MQTT Resolution"
+	sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+	sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+	sock.sendto(MESSAGE,(UDP_IP,UDP_PORT))
+	data,addr = sock.recvfrom(1024)
+	return data
