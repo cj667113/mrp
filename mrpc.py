493fec2a7c8bd6bb84006891b217dce57d3044cc
Chris Johnston <40300884+cj667113@users.noreply.github.com>
2021-08-30 10:41:08 -0500

    Update mrpc.py

--- mrpc.py
+++ mrpc.py
@@ -1,10 +1,10 @@
-import socket
+import usocket as socket
 def mrpc():
 	UDP_IP="255.255.255.255"
 	UDP_PORT=5005
 	MESSAGE=b"MQTT Resolution"
 	sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-	sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+	sock.settimeout(2)
 	sock.sendto(MESSAGE,(UDP_IP,UDP_PORT))
 	data,addr = sock.recvfrom(1024)
 	return data
